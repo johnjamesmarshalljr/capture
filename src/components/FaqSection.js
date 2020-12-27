568233204c210952825fa67190700f8ebdd572ab
jerkzilla <johnjamesmarshalljr@gmail.com>
2020-12-27 15:06:17 -0600

    added faq section

---
+++ src/components/FaqSection.js
@@ -0,0 +1,81 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any Questions <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How Do I Start</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque dolor
+            distinctio quasi quaerat earum, officia exercitationem asperiores.
+            Cupiditate, enim! Voluptatum aliquid facilis itaque quae. Quasi
+            repellat ab quidem impedit vel.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi
+            cumque commodi aperiam mollitia perferendis exercitationem. Vitae
+            reiciendis saepe nesciunt ullam, quasi magnam earum sapiente tempora
+            ex non, quia exercitationem pariatur?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque dolor
+            distinctio quasi quaerat earum, officia exercitationem asperiores.
+            Cupiditate, enim! Voluptatum aliquid facilis itaque quae. Quasi
+            repellat ab quidem impedit vel.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi
+            cumque commodi aperiam mollitia perferendis exercitationem. Vitae
+            reiciendis saepe nesciunt ullam, quasi magnam earum sapiente tempora
+            ex non, quia exercitationem pariatur?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Payment Methods</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque dolor
+            distinctio quasi quaerat earum, officia exercitationem asperiores.
+            Cupiditate, enim! Voluptatum aliquid facilis itaque quae. Quasi
+            repellat ab quidem impedit vel.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi
+            cumque commodi aperiam mollitia perferendis exercitationem. Vitae
+            reiciendis saepe nesciunt ullam, quasi magnam earum sapiente tempora
+            ex non, quia exercitationem pariatur?
+          </p>
+        </div>
+      </div>
+      <div className="question">
+        <h4>Which Products Do You Offer</h4>
+        <div className="answer">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque dolor
+            distinctio quasi quaerat earum, officia exercitationem asperiores.
+            Cupiditate, enim! Voluptatum aliquid facilis itaque quae. Quasi
+            repellat ab quidem impedit vel.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi
+            cumque commodi aperiam mollitia perferendis exercitationem. Vitae
+            reiciendis saepe nesciunt ullam, quasi magnam earum sapiente tempora
+            ex non, quia exercitationem pariatur?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
